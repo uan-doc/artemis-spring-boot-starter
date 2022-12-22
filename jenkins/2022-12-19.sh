838fb8e9112535ca7f370201a8c4bae5db294dd0
Jesus Saad <jesus.saad1@gmail.com>
2022-12-22 10:28:54 -0300

    novo jenkins

---
+++ jenkins/2022-12-19.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/Users/Shared/ON3/git/devops/github/add_web_hook.sh uan-doc artemis-spring-boot-starter
+/Users/Shared/ON3/git/devops/jenkins/api/script_clone_default_build_job.sh 'artemis-spring-boot-starter' 'Infra'
